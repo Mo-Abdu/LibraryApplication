5e9e8d3795d3794de225c571b5c21f7c8f0c7982
MOHAMED <mohamed.ongao@gmail.com>
2024-04-23 09:50:37 +0200

    first commit
    Hi

---
+++ Objects/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace LibraryApplication.Objects
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public  string Author { get; set; }
+        public int NumberOfPage   { get; set; }
+
+        public Book(int id, string name, string author, int numberOfPage)
+        {
+            Id = id;
+            Name = name;
+            Author = author;
+            NumberOfPage = numberOfPage;
+        }
+    }
+}
